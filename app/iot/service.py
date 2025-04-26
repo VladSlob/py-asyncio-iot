6da183ce48c5066d5cdace754cc44d04c19f1880
VladSlob <141657806+VladSlob@users.noreply.github.com>
2025-04-26 09:40:07 +0300

    service.py

--- app/iot/service.py
+++ app/iot/service.py
@@ -9,6 +9,18 @@ def generate_id(length: int = 8) -> str:
     return "".join(random.choices(string.ascii_uppercase, k=length))
 
 
+# Protocol is very similar to ABC, but uses duck typing
+# so devices should not inherit for it (if it walks like a duck, and quacks like a duck, it's a duck)
+class Device(Protocol):
+    def connect(self) -> None:
+        ...  # Ellipsis - similar to "pass", but sometimes has different meaning
+
+    def disconnect(self) -> None:
+        ...
+
+    def send_message(self, message_type: MessageType, data: str) -> None:
+        ...
+
 
 class IOTService:
     def __init__(self) -> None:
